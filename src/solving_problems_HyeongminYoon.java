a3e496180c3303a811d38f14f9f570966bc9dbf6
HyungminYoon1 <156720686+HyungminYoon1@users.noreply.github.com>
2024-07-31 18:28:38 +0900

    feat: 문제 풀기 기능 수정

--- src/solving_problems_HyeongminYoon.java
+++ src/solving_problems_HyeongminYoon.java
@@ -1,4 +1,4 @@
-public class Main {
+public class solving_problems_HyeongminYoon {
     public static void main(String[] args) {
         InputAndView application = new InputAndView();
         application.showMain();
